cf172884867b0bbeb1be427ec5dc787919fbc673
delosiliana <dronixa@mail.ru>
2018-02-04 05:01:13 +0300

    add user view question

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,10 +2,15 @@ FactoryBot.define do
   factory :question do
     title "MyString"
     body "MyText"
+  end
 
-    factory :invalid_question do
-      title nil
-      body nil
-    end
+  factory :invalid_question, class: Question do
+    title nil
+    body nil
+  end
+
+  factory :questions, class: Question do
+    sequence(:title) { |n| "Title #{n}" }
+    sequence(:body) { |n| "MyText #{n}" }
   end
 end
