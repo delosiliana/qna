5caf375dc783ab7db2bc1cc8dd2116a99de4d834
delosiliana <dronixa@mail.ru>
2018-02-08 19:05:22 +0300

    add test model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,31 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it { should have_many(:answers) }
+  it { should have_many(:questions) }
+
+  let(:user) { create :user }
+  let(:question) { create(:question, user: user) }
+  let(:no_author) { create :user}
+  let(:answer) { create(:answer, question: question, user: user) }
+
+  describe '#author? method for question' do
+    it 'return true if user author resource' do
+      expect(user.author?(question)).to be true
+    end
+
+    it 'return false if user no author resource' do
+      expect(no_author.author?(question)).to be false
+    end
+  end
+
+  describe '#author? method for answer' do
+    it 'return true if user author resource' do
+      expect(user.author?(answer)).to be true
+    end
+
+    it 'return false if user no author resource' do
+      expect(no_author.author?(answer)).to be false
+    end
+  end
 end
