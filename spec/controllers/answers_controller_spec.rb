3faa409820f3dbc817f4288ce78c417cd349dcc0
delosiliana <dronixa@mail.ru>
2018-02-19 15:23:21 +0300

    rewrite answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,30 +2,35 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create :question }
+  let(:answer) {create(:answer, user: user, question: question) }
+  let(:invalid_params) { { answer: attributes_for(:invalid_answer), question_id: question } }
 
   describe 'POST #create' do
     login_user
+    let(:params) { { answer: attributes_for(:answer), question_id: question}}
 
     context 'with valid attributes' do
       it 'saves new answer in database' do
-        params = { answer: attributes_for(:answer), question_id: question }
         expect { post :create, params: params }.to change(question.answers, :count).by(1)
       end
 
+      it 'the answer belongs to user' do
+        expect { post :create, params: params }.to change(@user.answers, :count).by(1)
+      end
+
       it 'redirects to show view' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question}
+        post :create, params: params
         expect(response).to redirect_to assigns(:question)
       end
     end
 
     context 'with no valid attributes' do
       it 'doesnt save the answer' do
-        params = { answer: attributes_for(:invalid_answer), question_id: question }
-        expect { post :create, params: params }.to_not change(Answer, :count)
+        expect { post :create, params: invalid_params }.to_not change(Answer, :count)
       end
 
       it 'rerenders new view' do
-        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
+        post :create, params: invalid_params
         expect(response).to render_template 'questions/show'
       end
     end
