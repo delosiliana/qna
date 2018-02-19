01abf6319177c58bd8df3aca33b1a69f47aa3f5c
delosiliana <dronixa@mail.ru>
2018-02-19 14:39:47 +0300

    rewrite answer acceptance test and add errors view

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,6 +7,7 @@ class AnswersController < ApplicationController
     @answer.user = current_user
 
     if @answer.save
+      flash[:notice] = 'Your answer successfully created.'
       redirect_to @question
     else
       render 'questions/show'
--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -16,7 +16,23 @@ feature 'Create answer for question', %q{
     fill_in 'Your answer', with: 'Answer text'
     click_on 'Answer'
 
-    expect(page).to have_content 'Answer text'
+    expect(page).to have_content 'Your answer successfully created'
+  end
+
+  scenario 'The authenticated user tries to create a invalid answer' do
+    sign_in(user)
+
+    visit question_path(question)
+    click_on 'Answer'
+
+    expect(page).to have_content "Body can't be blank"
+  end
+
+  scenario 'Non-authenticated user ties user create answer' do
+    visit question_path(question)
+    click_on 'Answer'
+
+    expect(page).to have_content 'You need to sign in'
   end
 end
 
