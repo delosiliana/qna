5d1a98eb900a5866972320f084290d2d2fe781cd
delosiliana <dronixa@mail.ru>
2018-02-19 15:01:33 +0300

    add acceptance test questions and answer for question

---
+++ spec/acceptance/user_can_view_question_and_answer_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can view question and answer', %q{
+  To view the question with answers
+} do
+
+  given(:question) {create :question}
+  given!(:answers) { create_list(:answer, 3, question: question) }
+
+  scenario 'View question with answers' do
+    visit question_path(question)
+    
+    expect(page).to have_content question.title
+
+    answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+end
