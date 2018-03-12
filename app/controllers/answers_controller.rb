2fdcdd2c8a2062fba2124be4f20fcdb5c18cf5ab
delosiliana <dronixa@mail.ru>
2018-03-12 17:50:22 +0300

    add edit, update answers AJAX test and views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,16 +1,23 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_answer, only: [:update, :destroy]
 
   def create
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.create(answer_params)
+    @answer = @question.answers.new(answer_params)
     @answer.user = current_user
 
     @answer.save
   end
 
+  def update
+    if current_user.author?(@answer)
+      @answer.update(answer_params)
+      @question = @answer.question
+    end
+  end
+
   def destroy
-    @answer = Answer.find(params[:id])
     if current_user.author?(@answer)
       @answer.destroy
     end
@@ -21,5 +28,9 @@ class AnswersController < ApplicationController
 
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:id])
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -36,6 +36,33 @@ RSpec.describe AnswersController, type: :controller do
     end
   end
 
+  describe 'PATCH #update' do
+    login_user
+    let!(:answer) { create(:answer, question: question) }
+    let!(:answer_author) { create(:answer, question: question, user: @user) }
+
+    it 'assigns the requested answer to @answer' do
+      patch :update, params: { id: answer_author, question_id: question, answer: attributes_for(:answer), format: :js }
+      expect(assigns(:answer)).to eq answer_author
+    end
+
+    it 'assigns  the question' do
+      patch :update, params: { id: answer_author, question_id: question, answer: attributes_for(:answer), format: :js }
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'changes answer attributes' do
+      patch :update, params: { id: answer_author, answer: { body: 'new body' },question_id: question, format: :js }
+      answer_author.reload
+      expect(answer_author.body).to eq 'new body'
+    end
+
+    it 'render update template' do
+      patch :update, params: { id: answer_author, question_id: question, answer: attributes_for(:answer), format: :js }
+      expect(response).to render_template :update
+    end
+  end
+
   describe 'DELETE #destroy' do
     login_user
 
