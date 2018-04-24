5b0e4874cc1d86d895be185badf7357a3e614a5e
delosiliana <dronixa@mail.ru>
2018-04-24 10:34:53 +0300

    fix3

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -33,21 +33,29 @@ RSpec.describe Answer, type: :model do
     end
   end
 
-  context '.vote' do
+  describe '#vote' do
     it 'change votes count' do
       expect{ another_answer.vote(user, -1) }.to change(Vote, :count).by 1
     end
 
-    it 'created vote have correct params' do
-      vote = another_answer.vote(user, -1)
+    context 'created vote have correct params' do
+      before { vote = another_answer.vote(user, -1) }
 
-      expect(vote.votable).to eq another_answer
-      expect(vote.user_id).to eq user.id
-      expect(vote.count).to eq -1
+      it 'belongs to answer' do
+        expect(vote.votable).to eq another_answer
+      end
+
+      it 'belongs to user' do
+        expect(vote.user_id).to eq user.id
+      end
+
+      it 'downvotes the count' do
+        expect(vote.count).to eq -1
+      end
     end
   end
 
-  context '.vote?' do
+  describe '#vote?' do
     let!(:vote_up) { create(:vote, :up, user: user, votable: answer) }
 
     it 'return true if user have the right to vote' do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -15,21 +15,33 @@ RSpec.describe Question, type: :model do
   let!(:question) { create(:question, user: user) }
   let(:another_question) { create(:question) }
 
-  context '.vote' do
+  describe '#vote' do
     it 'votes count' do
       expect{ another_question.vote(user, 1) }.to change(Vote, :count).by 1
     end
 
-    it 'create a vote with the desired parameters' do
-      vote = another_question.vote(user, -1)
-      expect(vote.votable_id).to eq another_question.id
-      expect(vote.votable_type).to eq 'Question'
-      expect(vote.user_id).to eq user.id
-      expect(vote.count).to eq -1
+    context 'create a vote with the desired parameters' do
+      before { @vote = another_question.vote(user, -1) }
+
+      it 'belongs to question' do
+        expect(@vote.votable_id).to eq another_question.id
+      end
+      
+      it 'has polymorphic association Question' do
+        expect(@vote.votable_type).to eq 'Question'
+      end
+
+      it 'belongs to user' do
+        expect(@vote.user_id).to eq user.id
+      end
+
+      it 'downvotes the count' do
+        expect(@vote.count).to eq -1
+      end
     end
   end
 
-  context '.vote?' do
+  describe '#vote?' do
     let!(:vote_up) { create(:vote, :up, user: user, votable: question) }
 
     it 'return true if user have the right to vote' do
