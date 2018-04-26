07be5024868491d6043ea88aa2414d8264faa670
delosiliana <dronixa@mail.ru>
2018-04-26 12:34:01 +0300

    fix vote_spec

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  validates :count, presence: true, inclusion: { in: [-1, 0, 1] }
+  validates :count, presence: true, inclusion: { in: [-1, 1] }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Vote, type: :model do
   it { should belong_to :user }
   it { should belong_to :votable }
   it { should validate_presence_of :count }
+  it { should validate_inclusion_of(:count).in_array [-1, 1] }
 end
