20173db374a71b19b7e8540db94a4637ba3dde33
delosiliana <dronixa@mail.ru>
2018-05-13 07:29:47 +0300

    Refactoring AttachmentsController with responders

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,13 +1,10 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
 
+  respond_to :js
+
   def destroy
     @attachment = Attachment.find(params[:id])
-    if current_user.author?(@attachment.attachable)
-      @attachment.destroy
-      flash[:notice] = 'Attachment deleted'
-    else
-      flash[:notice] = 'You can not delete attachment'
-    end
+    respond_with(@attachment.destroy) if current_user.author?(@attachment.attachable)
   end
 end
