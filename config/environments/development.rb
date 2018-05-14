582f146cc21706ceed9250995c3cfac660544e14
delosiliana <dronixa@mail.ru>
2018-05-15 00:35:20 +0300

    add gem letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,6 @@ Rails.application.configure do
   config.force_ssl = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener
 end
