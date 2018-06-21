37068f8773a5f13c59643ed5d9f3c8946afc83e1
delosiliana <dronixa@mail.ru>
2018-06-21 19:21:53 +0300

    add mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "qna_#{Rails.env}"
+  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.sendmail_settings = {
+    location: '/usr/sbin/sendmail',
+    arguments: '-i'
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'qna@production.com'}
+  config.action_mailer.default_url_options = { host: '77.244.215.181' }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
