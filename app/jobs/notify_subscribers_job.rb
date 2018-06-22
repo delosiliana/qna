9160a7c12956abe82893e686f386cabd5c9b5b6e
delosiliana <dronixa@mail.ru>
2018-06-22 20:43:32 +0300

    config deploy cable

--- app/jobs/notify_subscribers_job.rb
+++ app/jobs/notify_subscribers_job.rb
@@ -1,10 +1,9 @@
 class NotifySubscribersJob < ApplicationJob
   queue_as :default
 
-  def perform(answer)
-    question = answer.question
 
-    question.subscriptions.find_each do |subscription|
+  def perform(answer)
+    answer.question.subscriptions.find_each do |subscription|
       QuestionsMailer.notify(subscription.user, answer).deliver_later
     end
   end
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/home/deploy/qna'
 set :deploy_user, 'deploy'
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', '.env', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', '.env', 'config/secrets.yml', 'config/cable.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
